b428d9990a354a4c4a283f5e355130f128a2c390
hariz20 <hariz.izzuddin20@gmail.com>
2024-12-25 20:14:11 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aitoma_cctv',
+  webDir: 'dist'
+};
+
+export default config;
